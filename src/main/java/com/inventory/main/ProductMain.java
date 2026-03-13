198d3ab3ae327200b3d84094264714ee166460e9
Sriman992 <2400031281@kluniversity.in>
2026-03-13 22:29:44 +0530

    Updated ProductMain

--- src/main/java/com/inventory/main/ProductMain.java
+++ src/main/java/com/inventory/main/ProductMain.java
@@ -14,12 +14,19 @@ public class ProductMain {
         Transaction tx = session.beginTransaction();
 
         // INSERT
-        Product p1 = new Product("Laptop","Dell i5",55000,10);
-        Product p2 = new Product("Mouse","Wireless Mouse",500,50);
+        Product p1 = new Product("Laptop","Electronics",55000,10);
+        Product p2 = new Product("Mouse","Electronics",500,50);
+        Product p3 = new Product("Keyboard","Electronics",1500,25);
+        Product p4 = new Product("Monitor","Electronics",12000,15);
+        Product p5 = new Product("Phone","Electronics",30000,8);
+        Product p6 = new Product("Tablet","Electronics",25000,6);
 
         session.save(p1);
         session.save(p2);
-
+        session.save(p3);
+        session.save(p4);
+        session.save(p5);
+        session.save(p6);
         tx.commit();
 
         // RETRIEVE
